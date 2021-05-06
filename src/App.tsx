55122f3c038de3ddd9b6281e19a8d87b6141e25d
lucotmo <lucotmo@gmail.com>
2021-05-06 10:10:00 -0500

    chore: add webpack and config initial project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import './styles.css'
+
+export const App = () => {
+  return (
+    <div>
+      <h1>Initial project</h1>
+    </div>
+  )
+}
---
+++ webpack/paths.js
@@ -0,0 +1,10 @@
+const path = require('path')
+
+module.exports = {
+  srcDir: path.resolve(__dirname, '../', 'src'),
+  publicDir: path.resolve(__dirname, '../', 'build'),
+  imagesFolder: 'images',
+  fontsFolder: 'fonts',
+  cssFolder: 'css',
+  jsFolder: 'js',
+}
