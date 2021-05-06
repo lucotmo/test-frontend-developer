b26040d33c14da9d908380305dc177adcb8a37ce
lucotmo <lucotmo@gmail.com>
2021-05-06 10:28:26 -0500

    feat: component Input

--- src/components/ui/inputs/Input/Input.tsx
+++ src/components/ui/inputs/Input/Input.tsx
@@ -1,11 +1,29 @@
+import React from 'react';
 import './Input.style.css'
 
-const Input = () => {
+type InputProps = (props: {
+  type: string;
+  placeholder?: string;
+  name: string;
+  onChange: React.ReactEventHandler;
+  value: string | number;
+}) => React.ReactElement
+
+const Input = React.memo<InputProps>(({ type, placeholder, name, onChange, value }) => {
   return (
-    <div>
-      <h1>Input</h1>
+    <div className="content">
+      <label htmlFor={name}>{placeholder}</label>
+      <input
+        id={name}
+        className="input"
+        type={type}
+        placeholder={placeholder}
+        name={name}
+        onChange={onChange}
+        value={value}
+      />
     </div>
   )
-}
+})
 
 export default Input
