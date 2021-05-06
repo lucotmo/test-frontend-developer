9fb3d6e8c00322a7dcc2446a44ed164feee962f7
lucotmo <lucotmo@gmail.com>
2021-05-06 10:29:34 -0500

    feat: component InputSelect

--- src/components/ui/inputs/InputSelect/InputSelect.tsx
+++ src/components/ui/inputs/InputSelect/InputSelect.tsx
@@ -1,10 +1,30 @@
+import React from 'react'
 import './InputSelect.style.css'
-const InputSelect = () => {
+
+type InputSelectProps = (props: {
+  value?: string | number | undefined;
+  name: string;
+  onChange: React.ChangeEventHandler<HTMLSelectElement>;
+}) => React.ReactElement
+
+const InputSelect = React.memo<InputSelectProps>(({ value, name, onChange }) => {
   return (
-    <div>
-      <h1>Input Select</h1>
+    <div className="content">
+      <label htmlFor={name}>Edad</label>
+      <select
+        value={value}
+        className="inputSelect"
+        name={name}
+        onChange={onChange}
+        id={name}
+      >
+        <option value="DEFAULT">Selecciona Edad</option>
+        {[...Array(83)].map((x, i) =>
+          <option value={`${i + 18}`} key={i + 18} >{i + 18} años</option>
+        )}
+      </select>
     </div>
   )
-}
+})
 
 export default InputSelect
