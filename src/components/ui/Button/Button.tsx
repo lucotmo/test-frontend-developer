b6d8c85b6fa3d76ae9608bfe464b84b31707f8ba
lucotmo <lucotmo@gmail.com>
2021-05-06 10:26:40 -0500

    feat: component Button

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -1,10 +1,21 @@
+import React from 'react';
 import './Button.style.css'
 
-const Button = () => {
+type ButtonProps = (props: {
+  type?: 'submit' | 'button';
+  name: String;
+  onClick?:
+    | ((event: React.MouseEvent<HTMLButtonElement>) => void)
+    | undefined;
+}) => React.ReactElement
+
+const Button: ButtonProps = ({ type = 'button', name, onClick }) => {
   return (
-    <div>
-      <h1>Button</h1>
-    </div>
+    <button
+      className="button"
+      type={type}
+      onClick={onClick}
+    >{name}</button>
   )
 }
 
