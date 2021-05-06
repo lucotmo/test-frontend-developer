241d3b5f4365b020c3ebbcf39a738949607e9198
lucotmo <lucotmo@gmail.com>
2021-05-06 10:32:17 -0500

    feat: component Modal

--- src/components/ui/Modal/Modal.tsx
+++ src/components/ui/Modal/Modal.tsx
@@ -1,9 +1,23 @@
-import 'Modal.style.css'
+import './Modal.style.css'
 
-const Modal = () => {
+type ModalProps = (props: {
+  onClick?:
+    | ((event: React.MouseEvent<HTMLButtonElement>) => void)
+    | undefined;
+}) => React.ReactElement
+
+const Modal: ModalProps = ({ onClick }) => {
   return (
-    <div>
-      <h1>Modal</h1>
+    <div className="modalContainer">
+      <div className="modal">
+        <h2>Datos Enviados</h2>
+        <p className="modal__message">Tu información fue enviada con <strong>éxito</strong>, estaremos en contacto contigo</p>
+        <button 
+          type="button"
+          className="modal__button"
+          onClick={onClick} 
+        >Ok</button>
+      </div>
     </div>
   )
 }
